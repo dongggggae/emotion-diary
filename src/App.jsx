c49c71e53d592b5d4294d2694677281491e41473
김동현 <doyn0822@hiconsy.com>
2024-04-11 15:38:15 +0900

    공통 이모티콘 추가

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,33 @@ import Edit from './pages/Edit';
 import MyButton from './components/MyButton';
 import MyHeader from './components/MyHeader';
 
+import { getEmotionImage } from './util/get-emotion';
+
 function App() {
-	return (
-		<BrowserRouter>
-			<div className="App">
-				<MyHeader headText={'App'} leftChild={<MyButton text={'왼쪽 버튼'} onClick={() => alert('a')} />} rightChild={<MyButton text={'오른쪽 버튼'} onClick={() => alert('a')} />} />
-				{/* <img src={`${import.meta.env.BASE_URL}assets/emotion1.png`} alt="감정이모티콘" /> */}
-				<h2>App.js</h2>
-				<MyButton type={'positive'} text="asd" onClick={() => alert('click')} />
-				<MyButton type={'negative'} text="asd" onClick={() => alert('click')} />
-				<MyButton text="asd" onClick={() => alert('click')} />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/new" element={<New />} />
-					<Route path="/edit" element={<Edit />} />
-					<Route path="/diary/:id" element={<Diary />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <MyHeader
+          headText={'App'}
+          leftChild={<MyButton text={'왼쪽 버튼'} onClick={() => alert('a')} />}
+          rightChild={<MyButton text={'오른쪽 버튼'} onClick={() => alert('a')} />}
+        />
+
+        <h2>App.js</h2>
+
+        <MyButton type={'positive'} text="asd" onClick={() => alert('click')} />
+        <MyButton type={'negative'} text="asd" onClick={() => alert('click')} />
+        <MyButton text="asd" onClick={() => alert('click')} />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
